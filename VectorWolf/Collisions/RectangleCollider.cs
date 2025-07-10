595b3cc560789275a43920a9b3c36b340ed64bab
cacticrown <nathan.kichler@outlook.com>
2025-07-10 20:54:07 +0200

    basic collision

---
+++ VectorWolf/Collisions/RectangleCollider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+using VectorWolf.Graphics;
+
+namespace VectorWolf.Collisions;
+
+public class RectangleCollider : Component
+{
+    public Vector2 Size = Vector2.Zero;
+    public Vector2 Offset = Vector2.Zero;
+
+    public bool CollideWith(RectangleCollider other) => GetRectangle().Intersects(other.GetRectangle());
+
+    public Rectangle GetRectangle()
+    {
+        return new Rectangle(
+            (int)(Entity.Position.X + Offset.X),
+            (int)(Entity.Position.Y + Offset.Y),
+            (int)Size.X,
+            (int)Size.Y
+        );
+    }
+}
