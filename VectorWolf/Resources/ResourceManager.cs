248619060c960a8a51424cdfe1dc32e92df07519
cacticrown <nathan.kichler@outlook.com>
2025-07-11 15:04:17 +0200

    added resourceManager

---
+++ VectorWolf/Resources/ResourceManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using VectorWolf.Graphics;
+
+namespace VectorWolf.Resources;
+
+public static class ResourceManager
+{
+    public static Dictionary<string, Texture2D> Textures { get; } = new Dictionary<string, Texture2D>();
+
+    public static Texture2D LoadTexture(string path)
+    {
+        if(Textures.TryGetValue(path, out var texture))
+            return texture;
+
+        Stream stream = File.OpenRead(path);
+        texture = Texture2D.FromStream(RenderContext.GraphicsDevice, stream);
+
+        Textures.Add(path, texture);
+        return texture;
+    }
+}
