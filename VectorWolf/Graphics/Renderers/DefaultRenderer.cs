36a46d329d3e81e29db67d4bc4bc223218c076e3
cacticrown <nathan.kichler@outlook.com>
2025-07-12 18:25:31 +0200

    Update DefaultRenderer.cs

--- VectorWolf/Graphics/Renderers/DefaultRenderer.cs
+++ VectorWolf/Graphics/Renderers/DefaultRenderer.cs
@@ -5,16 +5,23 @@ namespace VectorWolf.Graphics.Renderers;
 
 public class DefaultRenderer : Renderer
 {
+    public SpriteSortMode SortMode = SpriteSortMode.BackToFront;
+    public BlendState BlendState = BlendState.AlphaBlend;
+    public SamplerState SamplerState = SamplerState.PointClamp;
+    public DepthStencilState DepthStencilState = DepthStencilState.None;
+    public RasterizerState RasterizerState = RasterizerState.CullNone;
+    public Color BackgroundColor = Color.Black;
+
     public override void Render(Scene scene)
     {
-        RenderContext.GraphicsDevice.Clear(Color.CornflowerBlue);
+        RenderContext.GraphicsDevice.Clear(BackgroundColor);
 
         RenderContext.SpriteBatch.Begin(
-            sortMode: SpriteSortMode.BackToFront,
-            blendState: BlendState.AlphaBlend,
-            samplerState: SamplerState.PointClamp,
-            depthStencilState: DepthStencilState.None,
-            rasterizerState: RasterizerState.CullNone,
+            SortMode,
+            BlendState,
+            SamplerState,
+            DepthStencilState,
+            RasterizerState,
             transformMatrix: RenderContext.ActiveCamera.GetViewMatrix()
         );
 
